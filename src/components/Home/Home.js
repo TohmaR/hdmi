6f2875c9fd1866a1d0d9f6f65e18cbe91529ede6
Thomas Roux <praxyftw@gmail.com>
2022-03-20 21:21:58 +0100

    add animation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -36,7 +36,12 @@ function Home() {
             />
             <div className='home__gradient'></div>
             <div className="home__scroll"> 
-               <ArrowDownSvg />
+                <div className="home__scroll__container">
+                    <img className="home__scroll__image" src="images/scroll-down.png" alt="scroll-down" />
+                    <svg className="home__scroll__arrow" width="34" height="20" viewBox="0 0 34 20" fill="none"xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2 2L17 17L32 2" stroke="#F7F0EE" strokeWidth="4" />
+                    </svg>
+                </div>
             </div>
             <div class="home__wave">
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
