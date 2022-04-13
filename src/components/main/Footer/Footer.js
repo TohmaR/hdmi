d0a178750dbd25a018c87e82065d236f1e2d2355
Thomas Roux <praxyftw@gmail.com>
2022-04-13 20:03:41 +0200

    add mobile social icon footer

--- src/components/main/Footer/Footer.js
+++ src/components/main/Footer/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
+import { DiscordSvg, TwitterSvg } from '../Icons/svg';
 import './Footer.css'
 
 function Footer() {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1024px)' })
   return (
     <div className="footer" data-scroll-section>
-        ©{(new Date().getFullYear())} HDMI. ALL RIGHT RESERVED.
+      {isTabletOrMobile
+      &&
+        <div className='footer__social'>
+            <a href="https://discord.gg/bNxJ9RwwZ2" target="_blank" rel="noreferrer"><DiscordSvg /></a>
+            <a href="https://twitter.com/HDMIping" target="_blank" rel="noreferrer"><TwitterSvg /></a>
+        </div>
+      }
+      ©{(new Date().getFullYear())} HDMI. ALL RIGHT RESERVED.
     </div>
   )
 }
